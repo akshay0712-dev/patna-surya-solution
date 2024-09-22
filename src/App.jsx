60b1efc516bf5f6d68b7e03e59807357c8910bc0
AkshayGit <akshayrishu4@gmail.com>
2024-09-22 09:20:31 +0530

    add font family to whole webpage and add css to developer name

--- src/App.jsx
+++ src/App.jsx
@@ -219,8 +219,8 @@ function App() {
           Thank you 🙋‍♂️ for visiting Patna Saurya Solution Pvt. Ltd.; together,
           let's pave the way to a sustainable future!
         </div>
-        <div className="flex justify-center my-3 text-center text-xs md:text-sm text-[#777777]">
-        <p>For website-related issues, please reach out to our developer: <a href="mailto:akshayrishu4@gmail.com">Akshay Kumar</a></p>
+        <div className="flex justify-center my-3 text-center text-xs md:text-sm ">
+        <p className="text-[#777777]">For website-related issues, please reach out to our developer: <a href="mailto:akshayrishu4@gmail.com" className="font-bold text-[#967252] hover:scale-125 transition-transform duration-200 inline-block"> Akshay Kumar</a></p>
         </div>
       </footer>
     </>
